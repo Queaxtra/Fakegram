c1ce653ea1451695db1a6426a138df16095b4bc6
Real Queaxtra <60826916+Queaxtra@users.noreply.github.com>
2023-06-26 21:47:34 +0300

    Add files via upload

--- src/components/Info.svelte
+++ src/components/Info.svelte
@@ -1,4 +1,4 @@
-<div class="mx-10 md:mx-32 lg:mx-32 mt-52 transition-all duration-300">
+<div class="mx-5 md:mx-32 lg:mx-32 mt-52 transition-all duration-300">
     <h2 class="text-2xl md:text-4xl lg:text-4xl text-white/80">How is it working?</h2>
     <p class="text-sm md:text-lg lg:text-lg w-80 md:w-[30rem] lg:w-[30rem] text-white/50">You can create your own fake post with our system. So how? <br> Click on "Create" button at the top. Then fill in the sections on the side of the page that appears. And it finished. Making fake posts is that easy.</p>
 </div>
