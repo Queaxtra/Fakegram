f75d9f4b69e29b5d23937ca90818a020ed2ffdda
Real Queaxtra <60826916+Queaxtra@users.noreply.github.com>
2023-06-26 21:34:54 +0300

    Update Header.svelte

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,9 +3,6 @@
 
     <div class="relative mt-60">
         <p class="text-3xl mx-5 text-white/80">Ready to create a fake post for Instagram?</p>
-        <div>
-            <img class="w-40 md:w-48 lg:w-48 ml-44 md:float-right lg:float-right opacity-50" src="https://i.hizliresim.com/fqtg3bj.png" alt="arrow">
-        </div>
         <div class="grid grid-cols-1">
             <a href="/create" class="p-2 transition-all duration-300 bg-[#363636] hover:bg-[#363636]/50 rounded relative ml-32 md:ml-5 lg:ml-5 w-40 mt-5"><i class="ri-magic-fill relative top-0.5"></i> Create</a>
         </div>
